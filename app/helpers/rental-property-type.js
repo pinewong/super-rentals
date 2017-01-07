bcf4449829eba432ad898c9b577218998bee30cb
PineWong <pinewong@163.com>
2017-01-07 16:05:55 +0800

    Add google maps.

---
+++ app/helpers/rental-property-type.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+const communityPropertyTypes = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
+export function rentalPropertyType([type]/*, hash*/) {
+  if (communityPropertyTypes.includes(type)) {
+    return 'Community';
+  }
+}
+
+export default Ember.Helper.helper(rentalPropertyType);
